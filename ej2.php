608a713d226de7051c50def55a4fc21ba4ede27b
daw2-herrera21 <71894765+daw2-herrera21@users.noreply.github.com>
2020-10-19 01:06:49 +0200

    Create ej2.php

---
+++ ej2.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+
+<footer>
+    <div style="text-align: center">
+
+    <p>Personal e-mail address: herreracorreatoni@fpllefia.com<br/></p>
+    <p>Site developed by: Toni Herrera Correa.</p>
+
+    </div>
+</footer>
+</body>
+</html>
